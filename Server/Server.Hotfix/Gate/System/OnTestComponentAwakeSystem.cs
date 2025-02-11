a778b24a81fb41aff862b5f223d9ad4e0a9f4b41
Stone <569812422@qq.com>
2025-02-11 20:03:06 +0800

    4

---
+++ Server/Server.Hotfix/Gate/System/OnTestComponentAwakeSystem.cs
@@ -0,0 +1,20 @@
+﻿using Fantasy;
+using Fantasy.Entitas.Interface;
+using Server.Entity.Gate.Component;
+using Server.Entity.Gate.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Hotfix.Gate.Model
+{
+    public class OnTestComponentAwakeSystem : AwakeSystem<TestComponent>
+    {
+        protected override void Awake(TestComponent self)
+        {
+            Log.Debug($"TestComponent {self.RuntimeId} is awake");
+        }
+    }
+}
